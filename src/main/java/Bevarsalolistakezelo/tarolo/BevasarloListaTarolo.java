b7b1f798149e15a99aadc5cf6e4a8ca68b4fac0f
Patrikvagyok20 <119357226+Patrikvagyok20@users.noreply.github.com>
2026-05-06 08:53:25 +0200

    Delete src/main directory

--- src/main/java/Bevarsalolistakezelo/tarolo/BevasarloListaTarolo.java
+++
@@ -1,7 +0,0 @@
-package Bevarsalolistakezelo.tarolo;
-
-import Bevarsalolistakezelo.entitas.BevasarloLista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BevasarloListaTarolo extends JpaRepository<BevasarloLista, Long> {
-}
